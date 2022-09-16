81a7893b10513cfeaa9e163a9a4489572dfc8ed6
Astmik <113307781+Astmik@users.noreply.github.com>
2022-09-16 13:47:18 +0300

    Приветствие и цвет

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Вы на ветке develop. Добро пожаловать!
+          Вы на ветке develop. Are you welcome!
         </p>
         <p>
           Редактируйте <code>src/App.js</code> и сохраните для обновления.
